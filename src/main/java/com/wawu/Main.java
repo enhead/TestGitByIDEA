ab5f927136e4f65ae72dc471d8787393505801fa
wawu <2790440179@qq.com>
2024-03-24 23:20:46 +0800

    第3次提交

--- src/main/java/com/wawu/Main.java
+++ src/main/java/com/wawu/Main.java
@@ -9,5 +9,8 @@ public class Main {
         System.out.println("Hello git");
         System.out.println("Hello git");
         System.out.println("Hello git");
+
+        System.out.println("Hello git");
+        System.out.println("Hello git");
     }
 }
