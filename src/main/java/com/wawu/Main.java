24662f5de70f807fe48193729a70a57316d58323
wawu <2790440179@qq.com>
2024-03-24 23:42:15 +0800

    当前是被new_branch提交

--- src/main/java/com/wawu/Main.java
+++ src/main/java/com/wawu/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello git");
+        System.out.println("当前行被new_branch分支修改");
         System.out.println("Hello git");
         System.out.println("Hello git");
 
